b005bd993307d2ce9aa3af4df22b10e35bbfca36
inspirezuza <inspire7220@gmail.com>
2023-11-12 12:08:02 +0700

    Break App Into Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,54 @@
-import "./styles.css"
+import { useState } from "react";
+import "./styles.css";
+import { NewTodoForm } from "./NewTodoForm";
 
-export default function App(){
-    return <>
-    <form className="new-item-form">
-      <div className="form-row">
-        <label htmlFor="item">New Item</label>
-        <input type="text" id="item"/>
-      </div>
-      <button className="btn">Add</button>
-    </form>
-    <h1 className="header">Todo List</h1>
-    <ul className="list">
-      <li>
-        <label>
-          <input type="checkbox"/>
-          Item 1
-        </label>
-        <button className="btn btn-danger">Delete</button>
-      </li>
-      <li>
-        <label>
-          <input type="checkbox"/>
-          Item 2
-        </label>
-        <button className="btn btn-danger">Delete</button>
-      </li>
-    </ul>
+export default function App() {
+  const [todos, setTodos] = useState([]);
+
+  function ToggleTodo(id, completed) {
+    setTodos((currentTodos) => {
+      return currentTodos.map((todo) => {
+        if (todo.id === id) {
+          return [...todo, completed];
+        }
+        return todo;
+      });
+    });
+  }
+
+  function deleteTodo(id) {
+    setTodos((currentTodos) => {
+      return currentTodos.filter((todo) => todo.id !== id);
+    });
+  }
+
+  return (
+    <>
+      <NewTodoForm />
+      <h1 className="header">Todo List</h1>
+      {todos.length === 0 && "No todos"}
+      <ul className="list">
+        {todos.map((todo) => {
+          return (
+            <li key={todo.id}>
+              <label>
+                <input
+                  type="checkbox"
+                  checked={todo.completed}
+                  onChange={(e) => ToggleTodo(todo.id, e.target.checked)}
+                />
+                {todo.title}
+              </label>
+              <button
+                onClick={() => deleteTodo(todo.id)}
+                className="btn btn-danger"
+              >
+                Delete
+              </button>
+            </li>
+          );
+        })}
+      </ul>
     </>
+  );
 }
